b9bbb747f5883aa9f04e35c56cdec68a40b9123f
Lola <lolahef@gmail.com>
2019-02-11 18:13:43 -0500

    Completed file_io.py

--- src/file_io.py
+++ src/file_io.py
@@ -8,11 +8,16 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
-# YOUR CODE HERE
+with open("foo.txt") as f:
+    read = f.read()
+    print(read)
+f.closed
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open("bar.txt","w+") as f:
+    f.write("'The day after the wedding, a Monday, was rainy. \n The rain began to fall just after midnight and continued without a stop till dawn. \n A soft, gentle rain that darkly dampened the spring earth and quietly stirred up the nameless creatures living in it.' Murakami (Sputnik Sweetheart)")
+f.closed
