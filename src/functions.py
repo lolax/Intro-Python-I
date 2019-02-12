a2f2ba0b208dc754a4f50149e7fbd8687af5fbbc
Lola <lolahef@gmail.com>
2019-02-12 13:16:48 -0500

    Updated equivalence to adhere to pep8 (from == to is)

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,6 @@
 # Write a function is_even that will return true if the passed-in number is even.
 def is_even(n):
-    return(n % 2 == 0)
+    return(n % 2 is 0)
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
